1e2586398555db00469b0fae3658103890ceeadc
richardli03 <rli@olin.edu>
2023-04-18 23:05:14 -0400

    some basic server open/closing built

--- src/server.c
+++ src/server.c
@@ -1,7 +1,26 @@
 #include "server.h"
 
+// Convenient error + exit program
+void error_and_exit(const char* error_msg) {
+  perror(error_msg);
+  // NOLINTNEXTLINE(concurrency-mt-unsafe)
+  exit(EXIT_FAILURE);
+}
+
 void open_server(void) {
-  // IMPLEMENTATION HERE
+  // SOCK_STREAM because we want accuracy > speed
+  int sock_desc = socket(AF_INET, SOCK_STREAM, 0);
+  if (sock_desc == -1) {
+    error_and_exit("Error with my socks!");
+  }
+  // Return the socket descriptor
+  return sock_desc;
+}
+void close_server(int socket_descriptor) {
+  // Replace this with your implementation.
+  if (close(socket_descriptor) == -1) {
+    error_and_exit("Couldn't close the server.");
+  }
 }
 
 void transmit_data(void) {
@@ -12,6 +31,13 @@ void clear_data_queue(void) {
   // IMPLEMENTATION HERE
 }
 
-void close_server(void) {
-  // IMPLEMENTATION HERE
+struct sockaddr_in socket_address(in_addr_t addr, in_port_t port) {
+  // Replace this with your implementation.
+  struct sockaddr_in foo;
+
+  foo.sin_family = AF_INET;
+  foo.sin_addr.s_addr = htonl(addr);
+  foo.sin_port = htons(port);
+
+  return foo;
 }
--- src/server.h
+++ src/server.h
@@ -18,9 +18,38 @@ typedef struct {
 } key_package;
 
 /**
- * Documentation here
+ * Attempt to open an IPv4 TCP socket.
+ *
+ * Try to create a TCP socket on IPv4, and return the socket descriptor if
+ * successful. If creating the socket is not successful, print an error message
+ * and exit the program without returning from the function.
+ *
+ * @return A socket descriptor corresponding to the new socket.
  */
 void open_server(void);
+/**
+ * Attempt to close an open socket.
+ *
+ * Try to close an existing socket given its descriptor. If closing the socket
+ * is not successful, print an error message and exit the program. In the case
+ * of an error, this function does not return.
+ *
+ * @param socket_descriptor The descriptor for the socket to close
+ */
+void close_server(int socket_descriptor);
+
+/**
+ * Return the sockaddr_in corresponding to an address and port.
+ *
+ * Given an address and port number, convert their representations to network
+ * byte order and assemble them into an approopriate sockaddr_in. The returned
+ * value can then be used to bind to or connect to a socket.
+ *
+ * @param addr The address to bind or connect the socket to, in host order.
+ * @param port The port number to bind or connect the socket to, in host order.
+ * @return A sockaddr_in structure to use with bind/connect, in network order.
+ */
+struct sockaddr_in socket_address(in_addr_t addr, in_port_t port);
 
 /**
  * Documentation here
@@ -31,8 +60,3 @@ void transmit_data(void);
  * Documentation here
  */
 void clear_data_queue(void);
-
-/**
- * Documentation here
- */
-void close_server(void);
