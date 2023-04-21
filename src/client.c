408b1396300cce6e12321adb9ac7f22bca351119
richardli03 <rli@olin.edu>
2023-04-21 16:33:40 -0400

    fixing one more client issue

--- src/client.c
+++ src/client.c
@@ -1,9 +1,12 @@
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 /**
  * Client side of server.
  */
 #include "client.h"
 
 void fileificate() {}
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
