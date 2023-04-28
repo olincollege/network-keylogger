1a68fad832d128d3f3f7ca3a09248e5610fd8c11
Lily Jiang <ljiang@olin.edu>
2023-04-28 16:17:38 -0400

    Keylogging works

--- README.md
+++ README.md
@@ -34,3 +34,30 @@ gcc tester.c -I/usr/include/libevdev-1.0 -levdev
 
 make sure to run with sudo permissions
 `sudo ./a.out`
+
+
+Note that each of the opened files corresponds to a different input event. So to get mouse data, you need to open a different file compared to if you want to get keyboard inputs. You can check the list of all events by running `sudo evtest` (note that you might have to install evtest first).
+
+For the specific device I'm running (Ubuntu), the list is as follows:
+
+/dev/input/event0:      Lid Switch
+/dev/input/event1:      Power Button
+/dev/input/event2:      Sleep Button
+/dev/input/event3:      AT Translated Set 2 keyboard
+/dev/input/event4:      Integrated_Webcam_HD: Integrate
+/dev/input/event5:      Dell WMI hotkeys
+/dev/input/event6:      DELL09C2:00 0488:121F Mouse
+/dev/input/event7:      PS/2 Generic Mouse
+/dev/input/event17:     Intel HID events
+/dev/input/event18:     Intel HID 5 button array
+/dev/input/event19:     DELL09C2:00 0488:121F Touchpad
+/dev/input/event20:     DELL09C2:00 0488:121F UNKNOWN
+/dev/input/event21:     Video Bus
+/dev/input/event22:     Video Bus
+/dev/input/event23:     DP-3
+/dev/input/event24:     HDA Intel PCH Headphone Mic
+/dev/input/event25:     HDA Intel PCH HDMI/DP,pcm=3
+/dev/input/event26:     HDA Intel PCH HDMI/DP,pcm=7
+/dev/input/event27:     HDA Intel PCH HDMI/DP,pcm=8
+/dev/input/event28:     HDA Intel PCH HDMI/DP,pcm=9
+/dev/input/event29:     HDA Intel PCH HDMI/DP,pcm=10
--- src/tester.c
+++ src/tester.c
@@ -13,6 +13,8 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+#include <poll.h>
+
 
 
 // If the device sends events faster than can be read, the kernel buffers will
@@ -52,16 +54,23 @@ int main(int argc, char **argv) {
 
     // open a device, as libevdev expects a file descriptor. You should have root permissions
     struct libevdev *dev;
-    int fd;
+    struct libevdev *keyboard_dev;
+    int fd;    // note that this will be for event0, which is the lid switch
     int rc;
 
     // open a new device from the file descriptor and initialize
     fd = open("/dev/input/event0", O_RDONLY|O_NONBLOCK);
-    if (fd < 0)
+    if (fd < 0) {
+        printf("yuck %d\n", EXIT_FAILURE);
         fprintf(stderr, "error opening file: %d %s\n", errno, strerror(errno));
+        return EXIT_FAILURE;
+    }
     rc = libevdev_new_from_fd(fd, &dev);
-    if (rc < 0)
+    if (rc < 0) {
         fprintf(stderr, "error with setting rc: %d %s\n", -rc, strerror(-rc));
+        fclose(data_storage);
+        return EXIT_FAILURE;
+    }
 
 
     // Use the getter functions to gather info about the device
@@ -71,51 +80,58 @@ int main(int argc, char **argv) {
         libevdev_get_id_product(dev));
 
     // store data into text file
-    fprintf(data_storage,"Device: %s | vendor: %x | product: %x\n",
-        libevdev_get_name(dev),
-        libevdev_get_id_vendor(dev),
-        libevdev_get_id_product(dev));
-
-
-    // check for E_REL first, then the actual axes. Allows us to skip libevdev_has_event_type()
-    if (libevdev_has_event_type(dev, EV_REL) &&
-        libevdev_has_event_code(dev, EV_REL, REL_X) &&
-        libevdev_has_event_code(dev, EV_REL, REL_Y) &&
-        libevdev_has_event_code(dev, EV_KEY, BTN_LEFT) &&
-        libevdev_has_event_code(dev, EV_KEY, BTN_MIDDLE) &&
-        libevdev_has_event_code(dev, EV_KEY, BTN_RIGHT)) {
-        printf("We have a mouse\n");
-    } else {
-        printf("1. %d\n", libevdev_has_event_type(dev, EV_REL));
-        printf("2. %d\n", libevdev_has_event_code(dev, EV_REL, REL_X));
-        printf("3. %d\n", libevdev_has_event_code(dev, EV_REL, REL_Y));
-        printf("4. %d\n", libevdev_has_event_code(dev, EV_REL, BTN_LEFT));
-        printf("5. %d\n", libevdev_has_event_code(dev, EV_REL, BTN_MIDDLE));
-        printf("6. %d\n", libevdev_has_event_code(dev, EV_REL, BTN_RIGHT));
+    if (data_storage != NULL) {
+        fprintf(data_storage, "Device: %s | vendor: %x | product: %x\n",
+            libevdev_get_name(dev),
+            libevdev_get_id_vendor(dev),
+            libevdev_get_id_product(dev));
     }
 
-    // get the next event and process it
-    struct input_event ev;
-
-    rc = libevdev_next_event(dev, LIBEVDEV_READ_FLAG_NORMAL, &ev);
+    // get keyboard inputs event file
+    int keyboard_fd = open("/dev/input/event3", O_RDONLY|O_NONBLOCK);
+    if (keyboard_fd < 0) {
+        fprintf(stderr, "error opening event3 file: %d %s\n", errno, strerror(errno));
+        return EXIT_FAILURE;
+    }
+    rc = libevdev_new_from_fd(keyboard_fd, &keyboard_dev);
     if (rc < 0) {
-        printf("value of rc: %d\n", rc);
-        if (rc != -EAGAIN)
-            fprintf(stderr, "error: %d %s\n", -rc, strerror(-rc));
-    else if (rc == LIBEVDEV_READ_STATUS_SYNC) {
-        printf("uhhh x2\n");
-        handle_syn_dropped(dev);
+        fprintf(stderr, "error with setting rc: %d %s\n", -rc, strerror(-rc));
+        fclose(data_storage);
+        return EXIT_FAILURE;
     }
-    else if (rc == LIBEVDEV_READ_STATUS_SUCCESS)
-        printf("We have an event!\n%d (%s) %d (%s) value %d\n",
-            ev.type, libevdev_event_type_get_name(ev.type),
-            ev.code, libevdev_event_code_get_name(ev.type, ev.code),
-            ev.value);
+
+    printf("does keyboard have key events? (1 = yes): %d\n", libevdev_has_event_type(keyboard_dev, EV_KEY));
+
+    while (1) {
+        struct input_event ev;
+
+        // other options: LIBEVDEV_READ_FLAG_NORMAL
+        rc = libevdev_next_event(keyboard_dev, LIBEVDEV_READ_FLAG_BLOCKING, &ev);
+        if (rc < 0) {
+            // note that this section runs when no event is occurring, NOT necessarily when there is an error
+            // printf("value of rc: %d\n", rc);
+            if (rc != -EAGAIN)
+                printf("1 error: %d %s\n", -rc, strerror(-rc));
+                // exit(0);
+        } else if (rc == LIBEVDEV_READ_STATUS_SYNC) {
+            printf("uhhh x2\n");
+            handle_syn_dropped(dev);
+        } else if (rc == LIBEVDEV_READ_STATUS_SUCCESS) {
+            // handle event here
+            printf("We have an event!\n%d (%s) %d (%s) value %d\n",
+                ev.type, libevdev_event_type_get_name(ev.type),
+                ev.code, libevdev_event_code_get_name(ev.type, ev.code),
+                ev.value);
+            // exit(1);
+        } else {
+            printf("unsure \n");
+        }
     }
 
     // Clean up
     libevdev_free(dev);
     close(fd);
+    close(keyboard_fd);
     fclose(data_storage);
 
 	return 0;
