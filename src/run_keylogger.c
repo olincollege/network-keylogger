318364845058d64d39ff05ee625c15e611c782f6
Aditi Vinod <avinod@olin.edu>
2023-05-05 00:32:32 -0400

    Adds file to run just the keylogger.

---
+++ src/run_keylogger.c
@@ -0,0 +1,14 @@
+/**
+ * Script created to run/test the keylogger (prior to unit tests).
+ */
+
+#include "keylogger.h"
+
+int main(void) {
+  key_package pack;
+  pack.keys_arr_size = 0;
+  log_device(&pack);
+  log_keys(&pack);
+  print_logged_keys(pack);
+  return 0;
+}
